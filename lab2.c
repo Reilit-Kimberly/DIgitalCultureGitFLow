403d6dad16b2432e458120de1c639f5ab010be61
Reilit <90870270+Reilit-Kimberly@users.noreply.github.com>
2022-02-15 14:19:18 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -8,5 +8,5 @@ int main()
     z1=((m-1)*pow(m,1./2)-(n-1)*pow(n,1./2))/(pow(pow(m, 3), 1./2)*pow(n,1./2)+n*m+pow(m, 2)-m);
     z2=(pow(m, 1./2)-pow(n,1./2))/m;
     printf("%lf %lf", z1,z2);
+    //test text
 }
-//test text
